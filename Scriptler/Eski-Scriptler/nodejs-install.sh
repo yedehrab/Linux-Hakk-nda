b4b6613d593a5da90b6327a11faf1556c6e405ac
Yunus Emre <yyunussemree@gmail.com>
2018-12-02 02:33:00 +0300

    Script ekleme  :dizzy:

--- Scriptler/Eski-Scriptler/nodejs-install.sh
+++ Scriptler/Eski-Scriptler/nodejs-install.sh
@@ -1,2 +1,3 @@
+# URL'deki ayarları bash'e ekleyerek apt paketlerine kaydetme
 wget -qO- https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
--- Scriptler/init-coder-apps.sh
+++ Scriptler/init-coder-apps.sh
@@ -3,6 +3,19 @@
 echo 'Sık kullandığım uygulamaların kurulumu'
 
 echo '--------------'
+echo 'VsCode Kurulumu'
+echo '--------------'
+cd /tmp
+mkdir vscode
+cd vscode
+wget http://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.29.1-1542309157_amd64.deb
+sudo dpkg -i install code_1.29.1-1542309157_amd64.deb
+sudo apt-get --fix-broken install
+sudo dpkg -i install code_1.29.1-1542309157_amd64.deb
+cd ..
+rm -rf vscode/
+
+echo '--------------'
 echo 'Git Kurulumu'
 echo '--------------'
 sudo apt-get install git
@@ -11,7 +24,7 @@ sudo apt-get install git
 echo '--------------'
 echo 'Nodejs (v11) Kurulumu'
 echo '--------------'
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+wget -qO https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install nodejs
 
 
@@ -19,4 +32,6 @@ echo '--------------'
 echo 'OpenJDK-11 Kurulumu (javac, java, vs.)'
 echo '--------------'
 sudo apt-get install openjdk-11-jdk
+
+
 
