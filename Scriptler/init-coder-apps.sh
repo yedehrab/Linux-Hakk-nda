0a9cce45acc167a1726dde837d449932bf1a494f
Yunus Emre <yyunussemree@gmail.com>
2018-12-02 20:45:49 +0300

    Yazılımcılar için gerekli appleri kuran script :star:

--- Scriptler/init-coder-apps.sh
+++ Scriptler/init-coder-apps.sh
@@ -1,37 +1,89 @@
 # Ubuntu-Cosmic için indirdiğim uygulamalar
 
-echo 'Sık kullandığım uygulamaların kurulumu'
+echo '------------------------------------------------'
+echo 'Yazılımcılar için gerekli uygulamaların kurulumu'
+echo '------------------------------------------------'
+echo 
+echo 'Paketlerin güncelleniyor.'
+echo '-------------------------'
 
-echo '--------------'
-echo 'VsCode Kurulumu'
-echo '--------------'
-cd /tmp
-mkdir vscode
-cd vscode
-wget http://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.29.1-1542309157_amd64.deb
-sudo dpkg -i install code_1.29.1-1542309157_amd64.deb
-sudo apt-get --fix-broken install
-sudo dpkg -i install code_1.29.1-1542309157_amd64.deb
-cd ..
-rm -rf vscode/
+sudo apt-get update
 
-echo '--------------'
-echo 'Git Kurulumu'
-echo '--------------'
-sudo apt-get install git
+echo '-----------------------------------'
+read -p "Git indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo '-----------------------------------'
+	echo
+	sudo apt-get install git
+fi
+
+echo '-----------------------------------'
+read -p "VsCode indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo '-----------------------------------'
+	echo
+	echo '- /tmp dizinine geçici olarak vscode klasörü açılıyor.'
+	cd /tmp
+	mkdir vscode
+	cd vscode
+
+	echo '- VsCode paketi http ile yükleniyor.'
+	wget http://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.29.1-1542309157_amd64.deb
+
+	echo '- VsCode kurulumu'
+	sudo dpkg -i /tmp/vscode/code_1.29.1-1542309157_amd64.deb
+	sudo apt-get --fix-broken install
+	sudo dpkg -i  code_1.29.1-1542309157_amd64.deb
+
+	echo '- Gereksiz dosyalar siliniyor.'
+	cd ..
+	rm -rf vscode/
+	sudo apt-get autoremove
+fi
+
+echo '-----------------------------------'
+read -p "Nodejs (v11) indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo '-----------------------------------'
+	echo
+	wget -qO https://deb.nodesource.com/setup_11.x | sudo -E bash -
+	sudo apt-get install nodejs
+fi
+
+echo '-----------------------------------'
+read -p "OpenJDK-11 (javac, java vs.) indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then	
+	echo '-----------------------------------'
+	sudo apt-get install openjdk-11-jdk
+fi
+
+echo '-----------------------------------'
+read -p "PostgreSQL indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo '-----------------------------------'
+	echo
+	sudo apt-get install postgresql
+fi
+
+echo '-----------------------------------'
+read -p "PostgreSQL JDBC Driver indirmek ister misin? [y/n] " # -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo '-----------------------------------'
+	echo
+	sudo apt-get install libpostgresql-jdbc-java libpostgresql-jdbc-java-doc
+	echo 'Dosyanın en altına bunu ekleyin'
+	echo 'export CLASSPATH=$CLASSPATH:/usr/share/java/postgresql-42.1.4.jar'
+	sudo gedit ~/.bashrc
+fi
 
 
-echo '--------------'
-echo 'Nodejs (v11) Kurulumu'
-echo '--------------'
-wget -qO https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install nodejs
 
-
-echo '--------------'
-echo 'OpenJDK-11 Kurulumu (javac, java, vs.)'
-echo '--------------'
-sudo apt-get install openjdk-11-jdk
 
 
 
